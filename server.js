2ebb6f15eb36d5798ae72aa6ce855771fb5ccdc3
pasha <returnto001@gmail.com>
2020-01-05 14:57:57 +0200

    Add User, Pin models.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('DB connected!'))
+.catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
--- typeDefs.js
+++ typeDefs.js
@@ -1,11 +1,31 @@
 const { gql } = require('apollo-server');
 
+//mongodb_admin    Nb45fOlDHCWi17Wo
+
 module.exports = gql`
   type User {
     _id: ID!
     name: String!
     email: String!
     picture: String!
+  }
+
+  type Pin {
+    _id: ID!
+    title: String!
+    content: String!
+    image: String!
+    latitude: Float!
+    longitude: Float!
+    createdAt: String!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    text: String!
+    createdAt: String!
+    author: User!
   }
 
   type Query {
